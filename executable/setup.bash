0ef4a543a666b54017a8989b91b5b24772238575
fedele7008 <56468984+fedele7008@users.noreply.github.com>
2022-10-05 18:18:45 -0400

    Update setup.bash

--- executable/setup.bash
+++ executable/setup.bash
@@ -3,7 +3,9 @@ PATH=${PATH}:"${MyCS_path}/executable/cs"
 PATH=${PATH}:"${MyCS_path}/user/script"
 
 for file in "$(ls ${MyCS_path}/user/setup/)"; do
-	source "${MyCS_path}/user/setup/${file}"
+	if [ -e "${MyCS_path}/user/setup/${file}" ]; then
+		source "${MyCS_path}/user/setup/${file}"
+	fi
 done
 
 alias cs="source cs"
