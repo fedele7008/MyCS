faca3c30d2b64b24b5d196f118d28166dd615931
John Yoon <fedelejohn7008@gmail.com>
2022-10-05 17:15:13 -0400

    uninstaller initiated

--- executable/setup.bash
+++ executable/setup.bash
@@ -1,3 +1,10 @@
 source "${MyCS_path}/executable/parseSetting" "setup.lang"
+PATH=${PATH}:"${MyCS_path}/executable/cs"
+PATH=${PATH}:"${MyCS_path}/user/script"
+
+for file in "$(ls ${MyCS_path}/user/setup/)"; do
+	source "${MyCS_path}/user/setup/${file}"
+done
+
 alias cs="source cs"
 echo "${script0000}"
