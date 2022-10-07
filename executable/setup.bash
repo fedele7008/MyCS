52521c8dba4940efca41a97ec7295abbe9eb1876
fedele7008 <56468984+fedele7008@users.noreply.github.com>
2022-10-06 20:31:06 -0400

    Update setup.bash

--- executable/setup.bash
+++ executable/setup.bash
@@ -2,7 +2,7 @@ source "${MyCS_path}/executable/parseSetting" "setup.lang"
 PATH=${PATH}:"${MyCS_path}/executable/cs"
 PATH=${PATH}:"${MyCS_path}/user/script"
 
-for file in "$(ls ${MyCS_path}/user/setup/)"; do
+for file in $(ls "${MyCS_path}/user/setup/"); do
 	if [ -e "${MyCS_path}/user/setup/${file}" ]; then
 		source "${MyCS_path}/user/setup/${file}"
 	fi
